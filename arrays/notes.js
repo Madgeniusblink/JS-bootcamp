3631eddecf985887d9db191d379abd36f5f203a4
Cristian R <madgeniusblink@gmail.com>
2019-03-01 13:54:41 -0700

    #2 notes + todo 1.0

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,19 +1,20 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
 
-//  for the end of the list
-console.log(notes.pop())
-notes.push('new notes')
+// //  for the end of the list
+// console.log(notes.pop())
+// notes.push('new notes')
 
 
-// for the start of the list
-console.log(notes.shift())
-notes.unshift('My first note')
+// // for the start of the list
+// console.log(notes.shift())
+// notes.unshift('My first note')
 
 
-// 
-notes.splice()
+// remove an item + add an item
+// notes.splice(1, 1, 'new item in the list from splice')
 
+notes[2] = 'This is now the new note 3'
 
 console.log(notes.length)
 console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,8 @@
 const  todo = ['workout', 'learn js', 'learn python', 'work', 'find new clients']
 
+todo.splice(2, 1)
+todo.push('New item to the end')
+todo.shift()
+
 console.log(`You have ${todo.length} todos on your list`)
-console.log(`todo: ${todo[todo.length - 5]}`)
-console.log(`todo: ${todo[todo.length - 1]}`)
+console.log(todo)
