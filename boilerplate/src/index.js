bf9e78331e236ffc056894a7f6e2db0f544015df
Madgeniusblink <madgeniusblink@gmail.com>
2019-04-19 21:57:31 -0400

    Clearn Boilerplate

--- boilerplate/src/index.js
+++
@@ -1,9 +0,0 @@
-import square, { add, name } from './utilities'
-import scream from './scream'
-
-console.log('index.js')
-console.log(add(32, 3))
-console.log(name)
-
-console.log(scream(name))
-console.log(square(10))
--- boilerplate/src/scream.js
+++
@@ -1,7 +0,0 @@
-const screamCris = (string) => {
-    return string.toUpperCase() + '!'
-}
-
-const scream = (string) => `${string.toUpperCase()}!`
-
-export { scream as default }
